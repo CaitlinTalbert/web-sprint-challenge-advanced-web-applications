5aaf5663aeb38af068b37ae78a08af2fe83ae80f
CaitlinTalbert <92820628+CaitlinTalbert@users.noreply.github.com>
2022-02-25 09:24:23 -0600

    login is redirecting to article

--- frontend/components/LoginForm.js
+++ frontend/components/LoginForm.js
@@ -1,40 +1,43 @@
-import React, { useState } from 'react'
-import PT from 'prop-types'
+import React, { useState } from "react";
+import PT from "prop-types";
 
 const initialFormValues = {
-  username: '',
-  password: '',
-}
+  username: "",
+  password: "",
+};
 export default function LoginForm(props) {
-  const [values, setValues] = useState(initialFormValues)
-  // ✨ where are my props? Destructure them here
+  const [values, setValues] = useState(initialFormValues);
+  const { login } = props;
 
-  const onChange = evt => {
-    const { id, value } = evt.target
-    setValues({ ...values, [id]: value })
-  }
+  const onChange = (evt) => {
+    const { id, value } = evt.target;
+    setValues({ ...values, [id]: value });
+  };
 
-  const onSubmit = evt => {
-    evt.preventDefault()
-    // ✨ implement
-  }
+  const onSubmit = (evt) => {
+    evt.preventDefault();
+    login(values);
+  };
 
   const isDisabled = () => {
     // ✨ implement
     // Trimmed username must be >= 3, and
     // trimmed password must be >= 8 for
     // the button to become enabled
-  }
+    //props.login.username.trim().length >= 3 &&
+    //props.login.password.trim().length >= 8;
+  };
 
   return (
     <form id="loginForm" onSubmit={onSubmit}>
-      <h2>Login</h2>
+      <h2 key={props.id}>Login</h2>
       <input
         maxLength={20}
         value={values.username}
         onChange={onChange}
         placeholder="Enter username"
         id="username"
+        key={props.id}
       />
       <input
         maxLength={20}
@@ -42,13 +45,16 @@ export default function LoginForm(props) {
         onChange={onChange}
         placeholder="Enter password"
         id="password"
+        key={props.id}
       />
-      <button disabled={isDisabled()} id="submitCredentials">Submit credentials</button>
+      <button disabled={isDisabled()} id="submitCredentials" key={props.id}>
+        Submit credentials
+      </button>
     </form>
-  )
+  );
 }
 
 // 🔥 No touchy: LoginForm expects the following props exactly:
 LoginForm.propTypes = {
   login: PT.func.isRequired,
-}
+};
