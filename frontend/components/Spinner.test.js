a2706bc8cd0ceaa6f09b68ff9ef0f9f785c1072f
CaitlinTalbert <92820628+CaitlinTalbert@users.noreply.github.com>
2022-02-25 15:48:07 -0600

    not sure about these tests

--- frontend/components/Spinner.test.js
+++ frontend/components/Spinner.test.js
@@ -1,9 +1,12 @@
 // Import the Spinner component into this file and test
 // that it renders what it should for the different props it can take.
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import Spinner from "./Spinner";
 
 test("sanity", () => {
-  expect(true).toBe(false);
+  expect(true).toBe(true);
 });
+
+//props is on and off?
+//spinner id is "spinner"
